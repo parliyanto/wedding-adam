44481ef60a901db4d2f1e5aa6bebcb43631edcfc
parliyanto <ahmadmaulanaparliyanto@gmail.com>
2025-10-27 22:21:00 +0700

    update penambahan konten & perubahan gambar bg parallax

--- src/app/Components/CoverSection.tsx
+++ src/app/Components/CoverSection.tsx
@@ -1,8 +1,22 @@
 "use client";
+import { Philosopher, Lora } from "next/font/google";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { FaEnvelope } from "react-icons/fa";
 import InvitationSection from "./InvitationSection";
+
+
+// import font philosopher
+const philosopher = Philosopher({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+// import font lora
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function WeddingCover() {
   const [isOpened, setIsOpened] = useState(false);
@@ -38,18 +52,18 @@ export default function WeddingCover() {
 
               {/* ✨ Text Content */}
               <div className="relative z-10 flex flex-col justify-center items-center h-full px-6">
-                <h3 className="text-sm tracking-widest mb-2">THE WEDDING OF</h3>
-                <h1 className="text-3xl md:text-4xl font-serif mb-4">
-                  ADAM & PASANGAN
+                <h3 className={`${lora.className} text-sm tracking-widest mb-2`}>THE WEDDING OF</h3>
+                <h1 className={`${philosopher.className} text-3xl md:text-4xl font-serif mb-4`}>
+                  ADAM & TASSYA
                 </h1>
-                <p className="text-sm mb-1">Kepada Yth.</p>
-                <p className="text-sm mb-4">Bapak/Ibu/Saudara/i</p>
-                <h2 className="text-xl font-semibold mb-6">Nama Tamu</h2>
+                <p className={`${lora.className} text-sm mb-1`}>Kepada Yth.</p>
+                <p className={`${lora.className} text-sm mb-4`}>Bapak/Ibu/Saudara/i</p>
+                <h2 className={`${lora.className} text-xl font-semibold mb-6`}>Nama Tamu</h2>
 
                 {/* 🎁 Tombol Buka Undangan */}
                 <button
                   onClick={handleOpenInvitation}
-                  className="bg-white text-gray-900 px-6 py-3 rounded-full flex items-center gap-2 hover:bg-gray-100 cursor-pointer"
+                  className={`${philosopher.className} bg-white text-gray-900 px-6 py-3 rounded-full flex items-center gap-2 hover:bg-gray-100 cursor-pointer`}
                 >
                   <FaEnvelope /> Buka Undangan
                 </button>
